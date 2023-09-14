c0cfc63f821a7c8e4ba8c7192ee1b0bdabbf04b8
Izz Hafeez <izzhafeez@gmail.com>
2023-09-15 01:37:00 +0800

    Improve oauth local storage

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <router-view/>
-  <button @click="login">Login Using Google</button>
-  <div v-if='googleAccessToken'>
-    <p>Google OAuth Token: {{ googleAccessToken }}</p>
+  <div v-if='this.token'>
+    <p>Google OAuth Token: {{ this.token }}</p>
+  </div>
+  <div>
+    <button @click="login">Login Using Google</button>
   </div>
 </template>
 
 <script>
 import { googleTokenLogin } from 'vue3-google-login';
-import { mapGetters } from 'vuex';
 export default {
   name: 'App',
-  computed: {
-    ...mapGetters(['googleAccessToken'])
+  data() {
+    return {
+      token: null
+    }
+  },
+  created() {
+    if (localStorage.getItem('tokenExpiry') > Date.now()) {
+      this.token = localStorage.getItem('googleAccessToken');
+    }
   },
   methods: {
     login() {
       googleTokenLogin().then(response => {
-        const googleAccessToken = response.access_token;
-
-        // Dispatch the action to store the Google OAuth token in Vuex
-        this.$store.dispatch('setGoogleToken', googleAccessToken);
-
         console.log("Handle response", response);
+        const accessToken = response.access_token;
+        localStorage.setItem('googleAccessToken', accessToken);
+        const expiry = Date.now() + 3600000
+        localStorage.setItem('tokenExpiry', expiry);
       });
     },
   },
--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue';
 import vue3GoogleLogin from 'vue3-google-login';
 import App from './App.vue';
 import router from './router';
-import store from './store';
 
 const app = createApp(App);
 const clientId = '785838083864-vljpaavprlvsq83slqjtf46oop9qtcfd.apps.googleusercontent.com';
@@ -11,6 +10,5 @@ app.use(vue3GoogleLogin, {
   clientId: clientId
 });
 app.use(router);
-app.use(store);
 
 app.mount('#app');
