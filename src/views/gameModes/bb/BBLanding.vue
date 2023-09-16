0476085e8e1f02e6b52256600a2538b789c67e30
Izz Hafeez <izzhafeez@gmail.com>
2023-09-16 21:07:35 +0800

    Edit BBLanding.vue: Add BB functionality

--- src/views/gameModes/bb/BBLanding.vue
+++ src/views/gameModes/bb/BBLanding.vue
@@ -1,4 +1,4 @@
-<!-- /csc -->
+<!-- /bb -->
 
 <template>
   <h1>{{ name }}</h1>
@@ -6,7 +6,7 @@
   <!-- <img :src="imgPath"/> -->
   <FormKit v-if="isAuthenticated" type="form" @submit="createRoom">
     <ContextForm/>
-    <CSCForm/>
+    <BBForm/>
   </FormKit>
   <PINInput v-else/>
 </template>
@@ -14,28 +14,28 @@
 <script>
 import axios from 'axios';
 import ContextForm from '@/components/ContextForm.vue';
-import CSCForm from '@/components/CSCForm.vue';
+import BBForm from '@/components/BBForm.vue';
 import { gameModeDict } from '../gameModes';
 import { getHeader, getUrl } from '@/services';
 import PINInput from '@/components/PINInput.vue';
 
 export default {
-  name: 'CSCLanding',
+  name: 'BBLanding',
   data() {
     return {
       isAuthenticated: false,
-      name: gameModeDict.csc.name,
-      description: gameModeDict.csc.description,
-      imgPath: `./${gameModeDict.csc.imgPath}`
+      name: gameModeDict.bb.name,
+      description: gameModeDict.bb.description,
+      imgPath: `./${gameModeDict.bb.imgPath}`
     };
   },
   created() {
     this.isAuthenticated = localStorage.getItem('expiry') > Date.now();
   },
-  components: { ContextForm, CSCForm, PINInput },
+  components: { ContextForm, BBForm, PINInput },
   methods: {
     async createRoom(fields) {
-      const url = getUrl('csc');
+      const url = getUrl('bb');
       const header = getHeader();
       axios.post(url, fields, { header })
         .then(response => {
@@ -53,7 +53,7 @@ export default {
         })
         .then(data => {
           if (!data) return;
-          this.$router.push(`csc/${data.id}`);
+          this.$router.push(`bb/${data.id}`);
         })
     }
   }
