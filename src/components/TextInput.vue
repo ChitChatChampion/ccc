c8b96ea89020eae907a5a13eb53036c93cdb5f42
Izz Hafeez <izzhafeez@gmail.com>
2023-09-19 23:43:10 +0800

    Edit TextInput.vue: I hate FormKit

---
+++ src/components/TextInput.vue
@@ -0,0 +1,31 @@
+<template>
+  <label v-if="!!label" :for="name" class="font-bold text-lg"><br/>{{ label }}<br/></label>
+  <input
+    :name="name"
+    v-model="value"
+    class="font-sans rounded-xl border-lrt border-2 p-2"
+    :placeholder="placeholder"/>
+</template>
+
+<script>
+export default {
+  name: 'TextInput',
+  data() {
+    return {
+      value: ''
+    }
+  },
+  props: {
+    placeholder: {
+      type: String,
+    },
+    label: {
+      type: String,
+    },
+    name: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
