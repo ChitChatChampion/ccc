e4937258b957450deb1c3abc3a6a8dc22d7f2e87
Izz Hafeez <izzhafeez@gmail.com>
2023-09-19 23:04:17 +0800

    Edit CSCLanding.vue: Add design implementation for various screens"

--- src/views/gameModes/csc/CSCLanding.vue
+++ src/views/gameModes/csc/CSCLanding.vue
@@ -1,63 +1,76 @@
 <!-- /csc -->
 
 <template>
-  <h1>{{ name }}</h1>
-  <em v-if="isAuthenticated">{{ createInstructions }}</em>
-  <em v-else>{{ description }}</em>
-  <!-- <img :src="imgPath"/> -->
-  <FormKit v-if="isAuthenticated" type="form" @submit="createRoom">
-    <ContextForm/>
-    <CSCForm/>
-  </FormKit>
-  <PINInput v-else/>
+  <div class="h-screen bg-gradient-to-b from-jr-light via-jr to-jr-dark z-0">
+    <NavBar backLink="/browse" text="Conversation Starter Cards"/>
+    <section id="padded" class="mx-10 px-5 grid gap-5">
+      <img :src="imgPath" class="x-auto max-h-80 z-10"/>
+      <h1 class="text-2xl text-light font-bold z-10">How to play:</h1>
+      <p class="text-light z-10">{{ instructions }}</p>
+      <PINInput isHorizontal="true"/>
+      <div>
+        <OrangeButton v-if="isAuthenticated" :onClick="e => this.$router.push('/csc/create')" text="Create Game"/>
+        <Login v-else redirect="/csc/create"/>
+      </div>
+    </section>
+
+    <div class="background-circle-csc bg-jr-light"></div>
+    <div class="background-diamond-csc bg-jr-light"></div>
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
-import ContextForm from '@/components/ContextForm.vue';
-import CSCForm from '@/components/CSCForm.vue';
+import NavBar from '@/components/NavBar.vue';
 import { gameModeDict } from '../gameModes';
-import { getHeader, getUrl } from '@/services';
 import PINInput from '@/components/PINInput.vue';
+import Login from '@/components/Login.vue';
+import OrangeButton from '@/components/OrangeButton.vue';
 
 export default {
   name: 'CSCLanding',
   data() {
     return {
-      isAuthenticated: false,
-      name: gameModeDict.csc.name,
-      description: gameModeDict.csc.description,
-      createInstructions: gameModeDict.csc.createInstructions,
-      imgPath: `./${gameModeDict.csc.imgPath}`
-    };
+      instructions: gameModeDict.csc.instructions,
+      imgPath: gameModeDict.csc.imgPath,
+      isAuthenticated: false
+    }
   },
   created() {
-    this.isAuthenticated = localStorage.getItem('expiry') > Date.now();
-  },
-  components: { ContextForm, CSCForm, PINInput },
-  methods: {
-    async createRoom(fields) {
-      const url = getUrl('csc');
-      const header = getHeader();
-      axios.post(url, fields, { header })
-        .then(response => {
-          switch (response.status) {
-            case 201:
-              return response.data;
-            default:
-              this.isInvalid = true;
-              this.$swal.fire({
-                icon: 'warning',
-                title: 'Oops...',
-                text: 'Something went wrong when creating your room!'
-              });
-          }
-        })
-        .then(data => {
-          if (!data) return;
-          this.$router.push(`csc/${data.id}`);
-        })
+    if (localStorage.getItem('expiry') > Date.now()) {
+      this.isAuthenticated = true;
     }
-  }
+  },
+  components: {
+    NavBar,
+    PINInput,
+    Login,
+    OrangeButton
+}
 }
 </script>
+
+<style scoped>
+.background-circle-csc {
+  position: absolute;
+  top: 70%;
+  left: 100%;
+  transform: translate(-50%, -50%);
+  width: 50vh; /* Adjust the size of the circle as needed */
+  height: 50vh; /* The width and height should be equal for a circle */
+  border-radius: 50%; /* Creates a circle by setting border-radius to 50% */
+  z-index: 0; /* Place the circle behind other content */
+  opacity: 20%;
+}
+
+.background-diamond-csc {
+  position: absolute;
+  top: 20%;
+  left: 0%;
+  transform: translate(-50%, -50%) rotate(45deg);
+  width: 50vmax; /* Adjust the size of the circle as needed */
+  height: 50vmax; /* The width and height should be equal for a circle */
+  border-radius: 4rem; /* Creates a circle by setting border-radius to 50% */
+  z-index: 0; /* Place the circle behind other content */
+  opacity: 20%;
+}
+</style>
