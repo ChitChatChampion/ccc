5bb1fdc7aed3cae9d7601e8a9cc8cb460d10f481
Izz Hafeez <izzhafeez@gmail.com>
2023-09-19 23:04:17 +0800

    Edit main.js: Add design implementation for various screens"

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import { plugin, defaultConfig } from '@formkit/vue';
 import vue3GoogleLogin from 'vue3-google-login';
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import config from '../formkit.config.js';
 import App from './App.vue';
 import router from './router';
 
@@ -18,7 +19,7 @@ app.use(vue3GoogleLogin, {
   clientId: clientId
 });
 app.use(router);
-app.use(plugin, defaultConfig);
+app.use(plugin, defaultConfig(config));
 app.use(VueSweetalert2);
 
 library.add(faArrowLeft);
