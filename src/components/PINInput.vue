ac44fc9ec7bb8bfc57ce421969109e04e0f185c7
Izz Hafeez <izzhafeez@gmail.com>
2023-09-19 23:04:17 +0800

    Edit PINInput.vue: Add design implementation for various screens"

--- src/components/PINInput.vue
+++ src/components/PINInput.vue
@@ -1,27 +1,37 @@
 <template>
-  <div class="flex mx-auto">
-    <FormKit type="form" submit-label="Join Game" @submit="submitPIN"
-      form-class="flex flex-col gap-3 text-center bg-light p-4 rounded-2xl mx-auto" :actions="false">
-      <FormKit type="text" name="roomId" placeholder="Game ID"
-        input-class="border-[#A7A7A7] border-2 p-3 font-sans font-bold rounded-xl" />
-      <FormKit type="submit" label="Join Game" outer-class="p-2 bg-cc rounded-xl font-sans font-bold text-light" />
-    </FormKit>
-  </div>
+  <form class="rounded-3xl gap-5 z-10 max-w-sm" :class="{ flex: isHorizontal, grid: !isHorizontal, 'bg-light': !isHorizontal, 'p-5': !isHorizontal }">
+    <input
+      v-model="roomId"
+      class="font-sans font-bold rounded-xl border-lrt border-2"
+      :class="{ 'p-2': isHorizontal, 'p-3': !isHorizontal }"
+      placeholder="Game ID"/>
+    <OrangeButton :onClick="submitPIN" text="Join Game"/>
+  </form>
 </template>
 
 <script>
 import { getUrl } from '@/services';
+import OrangeButton from './OrangeButton.vue';
 
 export default {
   name: 'PINInput',
+  props: {
+    isHorizontal: {
+      type: Boolean,
+      default: false
+    }
+  },
   data() {
     return {
-      isInvalid: false
-    };
+      roomId: ''
+    }
+  },
+  components: {
+    OrangeButton
   },
   methods: {
-    async submitPIN(fields) {
-      const roomId = fields.roomId;
+    async submitPIN() {
+      const roomId = this.roomId;
       const url = getUrl(`room/${roomId}`);
       console.log(url);
 
@@ -31,7 +41,6 @@ export default {
             case 200:
               return response.json();
             default:
-              this.isInvalid = true;
               this.$swal.fire({
                 icon: 'error',
                 title: 'Oops...',
@@ -43,6 +52,13 @@ export default {
           if (!data) return;
           const gameMode = data.type;
           this.$router.push(`${gameMode}/${roomId}`);
+        })
+        .catch(() => {
+          this.$swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Something went wrong!'
+          });
         });
     }
   }
