0ea3481b305e3e9169416b129857b265ec5ce103
Izz Hafeez <izzhafeez@gmail.com>
2023-09-19 23:43:10 +0800

    Edit SelectInput.vue: I hate FormKit

---
+++ src/components/SelectInput.vue
@@ -0,0 +1,38 @@
+<template>
+  <br/>
+  <label v-if="!!label" :for="name" class="font-bold text-lg">{{ label }}</label>
+  <br/>
+  <select
+    v-model="value"
+    class="p-2 rounded-xl border-lrt border-2">
+    <option disabled selected>{{ placeholder }}</option>
+    <option v-for="option in options" :value="option.value" :key="option.value">{{ option.label }}</option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'SelectInput',
+  data() {
+    return {
+      value: ''
+    }
+  },
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    placeholder: {
+      type: String,
+      required: true
+    },
+    options: {
+      required: true
+    },
+    label: {
+      type: String
+    }
+  }
+}
+</script>
