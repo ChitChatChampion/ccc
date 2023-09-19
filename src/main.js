072fc12571b4cef20933579aa73a0279ca1a69e2
Izz Hafeez <izzhafeez@gmail.com>
2023-09-19 19:29:41 +0800

    Add font-awesome as global variable in main

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import { plugin, defaultConfig } from '@formkit/vue';
 import vue3GoogleLogin from 'vue3-google-login';
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import App from './App.vue';
 import router from './router';
 
 import './index.css'
-
 
 const app = createApp(App);
 const clientId = '785838083864-vljpaavprlvsq83slqjtf46oop9qtcfd.apps.googleusercontent.com';
@@ -18,5 +20,8 @@ app.use(vue3GoogleLogin, {
 app.use(router);
 app.use(plugin, defaultConfig);
 app.use(VueSweetalert2);
+
+library.add(faArrowLeft);
+app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.mount('#app');
