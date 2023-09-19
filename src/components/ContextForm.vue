0c55a7dc817791f4dc9e4377c6a6147567e70316
Izz Hafeez <izzhafeez@gmail.com>
2023-09-19 23:43:10 +0800

    Edit ContextForm.vue: I hate FormKit

--- src/components/ContextForm.vue
+++ src/components/ContextForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <FormKit type="group" name="baseContext">
+  <!-- <FormKit type="group" name="baseContext">
     <FormKit
       type="text"
       name="purpose"
@@ -16,11 +16,51 @@
     />
     <FormKit
       type="select"
-      name="familiarity"
+      name="relationship"
       label="Familiarity"
       help="How familiar are the participants with each other?"
       placeholder="Select Familiarity"
       :options="['Strangers', 'Acquaintances', 'Friends', 'Close Friends']"
     />
-  </FormKit>
+  </FormKit> -->
+  <TextInput
+    name="purpose"
+    ref="purpose"
+    label="Purpose"
+    placeholder="e.g. Freshman orientation, First date etc."/>
+  <TextInput
+    name="description"
+    ref="description"
+    label="Description"
+    placeholder="e.g. University computing students from Singapore."/>
+  <SelectInput
+    name="relationship"
+    ref="relationship"
+    label="Relationship"
+    placeholder="Choose the relationship"
+    :options="[
+      { label: 'Strangers', value: 'STRANGERS' },
+      { label: 'Acquaintances', value: 'ACQUAINTANCES' },
+      { label: 'Friends', value: 'FRIENDS' },
+      { label: 'Close Friends', value: 'CLOSE FRIENDS' },
+    ]"/>
 </template>
+
+<script>
+import TextInput from './TextInput.vue';
+import SelectInput from './SelectInput.vue';
+
+export default {
+  name: 'ContextForm',
+  components: { TextInput, SelectInput },
+  methods: {
+    getValues() {
+      return {
+        purpose: this.$refs.purpose.value,
+        description: this.$refs.description.value,
+        relationship: this.$refs.relationship.value
+      }
+    }
+  }
+}
+</script>
