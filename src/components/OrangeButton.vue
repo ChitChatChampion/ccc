221db1d0822357174cb3d14a47d7f88a44b098a3
Izz Hafeez <izzhafeez@gmail.com>
2023-09-19 23:04:17 +0800

    Edit OrangeButton.vue: Add design implementation for various screens"

---
+++ src/components/OrangeButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button
+    type="button"
+    @click="onClick"
+    class="p-2 bg-cc font-bold text-light rounded-xl hover:bg-cc-light"
+  >{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: 'OrangeButton',
+  props: {
+    onClick: {},
+    text: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
