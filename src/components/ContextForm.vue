fcab3a8a70640ac73d8799a4aabb1480816338d1
Izz Hafeez <izzhafeez@gmail.com>
2023-09-19 23:52:11 +0800

    Edit ContextForm.vue: I'm a genius

--- src/components/ContextForm.vue
+++ src/components/ContextForm.vue
@@ -1,28 +1,4 @@
 <template>
-  <!-- <FormKit type="group" name="baseContext">
-    <FormKit
-      type="text"
-      name="purpose"
-      label="Purpose"
-      help="e.g. Freshman orientation, First date etc."
-      placeholder="Purpose of ice-breaker"
-    />
-    <FormKit
-      type="text"
-      name="description"
-      label="Description"
-      help="e.g. University computing students from Singapore."
-      placeholder="Purpose of ice-breaker"
-    />
-    <FormKit
-      type="select"
-      name="relationship"
-      label="Familiarity"
-      help="How familiar are the participants with each other?"
-      placeholder="Select Familiarity"
-      :options="['Strangers', 'Acquaintances', 'Friends', 'Close Friends']"
-    />
-  </FormKit> -->
   <TextInput
     name="purpose"
     ref="purpose"
