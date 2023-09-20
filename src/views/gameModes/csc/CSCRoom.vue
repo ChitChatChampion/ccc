b112b668598612c2290b0464048fa7dd23c82ce3
Jason Christopher <jason.christopher@u.nus.edu>
2023-09-21 03:06:26 +0800

    Add Vue Carousel Implementation

--- src/views/gameModes/csc/CSCRoom.vue
+++ src/views/gameModes/csc/CSCRoom.vue
@@ -1,16 +1,36 @@
 <!-- /csc/:id -->
 
 <template>
-  <h1>Conversation Starter Cards</h1>
-  Room ID: {{ this.$route.params.id }}<br/>
-  {{ instructions }}
-  <button v-if="cardIndex > 0" @click="cardIndex--">Previous</button>
-  <button v-if="cardIndex < cards.length - 1" @click="cardIndex++">Next</button>
-  <button @click="shuffle">Shuffle</button>
-  Card {{ cardIndex+1 }}: <div v-if="cards[cardIndex]">{{ cards[cardIndex]['text'] }}</div>
+  <div class="h-screen bg-gradient-to-b from-jr-light via-jr to-jr-dark z-0">
+    <NavBar backLink="/csc" text="Conversation Starter Cards" />
+    <section id="padded" class="flex flex-col mx-9 px-5 gap-5">
+      <div class="flex gap-2 text-light">
+        <span>Room ID:</span>
+        <span class="font-bold">{{ this.$route.params.id }}</span>
+      </div>
+      <button v-if="cardIndex > 0" @click="cardIndex--">Previous</button>
+      <button v-if="cardIndex < cards.length - 1" @click="next">Next</button>
+      <button @click="shuffle">Shuffle</button>
+      Card {{ cardIndex + 1 }}: <div v-if="cards[cardIndex]">{{ cards[cardIndex]['text'] }}</div>
+    </section>
+
+    <carousel ref="myCarousel" :items-to-show="1.3">
+      <slide :key="1">
+        <div></div>
+      </slide>
+      <slide v-for="card in cards" :key="card">
+        <div class="bg-light w-full text-left p-8 h-[22rem] rounded-3xl text-jr-dark text-[20px] font-medium">
+          {{ card.text }}
+        </div>
+      </slide>
+    </carousel>
+  </div>
 </template>
 
 <script>
+import NavBar from "@/components/NavBar.vue"
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide } from 'vue3-carousel'
 import { getUrl } from '@/services';
 import { gameModeDict } from '../gameModes';
 
@@ -19,7 +39,10 @@ export default {
   data() {
     return {
       cardIndex: 0,
-      cards: [],
+      cards: [
+        "Tell me about yourself",
+        "What is your favorite programming language",
+      ],
       instructions: gameModeDict.csc.instructions
     }
   },
@@ -48,7 +71,62 @@ export default {
   methods: {
     shuffle() {
       this.cards = this.cards.sort(() => Math.random() - 0.5);
+    },
+    next() {
+      this.$refs.myCarousel.next();
+    },
+    prev() {
+      this.$refs.myCarousel.prev();
     }
+  },
+  components: {
+    NavBar,
+    Carousel,
+    Slide,
   }
 }
 </script>
+
+
+<style scoped>
+.carousel__slide {
+  padding: 5px;
+}
+
+.carousel__viewport {
+  perspective: 2000px;
+}
+
+.carousel__track {
+  transform-style: preserve-3d;
+}
+
+.carousel__slide--sliding {
+  transition: 0.5s;
+}
+
+.carousel__slide {
+  opacity: 0.5;
+  transform: rotateY(-20deg) scale(0.9);
+}
+
+.carousel__slide--active~.carousel__slide {
+  transform: rotateY(20deg) scale(0.9);
+}
+
+.carousel__slide--prev {
+  opacity: 0.5;
+  transform: rotateY(-10deg) scale(0.9);
+}
+
+.carousel__slide--next {
+  opacity: 0.5;
+  transform: rotateY(10deg) scale(0.95);
+}
+
+.carousel__slide--active {
+  opacity: 1;
+  transform: rotateY(0) scale(1);
+}
+</style>
+Custom Navig
