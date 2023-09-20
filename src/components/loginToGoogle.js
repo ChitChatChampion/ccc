99514c1f68c0a531905bd2bcba5cc2f8601b4484
Izz Hafeez <izzhafeez@gmail.com>
2023-09-20 23:06:55 +0800

    Edit loginToGoogle.js: Improvements in the UI

---
+++ src/components/loginToGoogle.js
@@ -0,0 +1,18 @@
+import { googleTokenLogin } from 'vue3-google-login';
+
+const loginToGoogle = ({ redirect, router }) => {
+  googleTokenLogin().then(tokenResponse => {
+    localStorage.setItem('token', tokenResponse.access_token);
+
+    // get user data from Google
+    fetch(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${tokenResponse.access_token}`)
+      .then(userDataResponse => userDataResponse.json())
+      .then(data => {
+        localStorage.setItem('name', data.name);
+        localStorage.setItem('expiry', Date.now() + 3600000);
+        router.push(redirect);
+      });
+  });
+}
+
+export default loginToGoogle;
