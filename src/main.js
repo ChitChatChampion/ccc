eb3778067219aaa9879f5148a3ebb7d7d6adc3a0
Jason Christopher <jason.christopher@u.nus.edu>
2023-09-21 04:02:18 +0800

    Edit main.js: Add Clipboard Button

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faClipboard } from '@fortawesome/free-regular-svg-icons';
 import config from '../formkit.config.js';
 import App from './App.vue';
 import router from './router';
@@ -23,6 +24,10 @@ app.use(plugin, defaultConfig(config));
 app.use(VueSweetalert2);
 
 library.add(faArrowLeft);
+library.add(faChevronLeft);
+library.add(faChevronRight);
+library.add(faClipboard);
+
 app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.mount('#app');
