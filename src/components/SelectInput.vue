1ef8505ed062a44116dcab368322b18794545b43
Izz Hafeez <izzhafeez@gmail.com>
2023-09-20 23:06:55 +0800

    Edit SelectInput.vue: Improvements in the UI

--- src/components/SelectInput.vue
+++ src/components/SelectInput.vue
@@ -1,10 +1,8 @@
 <template>
-  <br/>
-  <label v-if="!!label" :for="name" class="font-bold text-lg">{{ label }}</label>
-  <br/>
+  <label v-if="!!label" :for="name" class="block text-gray-700 text-sm font-bold mb-2"><br/>{{ label }}<br/></label>
   <select
     v-model="value"
-    class="p-2 rounded-xl border-lrt border-2">
+    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
     <option disabled selected>{{ placeholder }}</option>
     <option v-for="option in options" :value="option.value" :key="option.value">{{ option.label }}</option>
   </select>
