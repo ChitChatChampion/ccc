f47a056e69837e24f8f53be4768afc811ff13a28
Izz Hafeez <izzhafeez@gmail.com>
2023-09-21 14:01:00 +0800

    Edit CSCForm.vue: Add logic for questions

--- src/components/CSCForm.vue
+++ src/components/CSCForm.vue
@@ -2,10 +2,13 @@
   <NumericInput
     name="number_of_questions"
     ref="number_of_questions"
-    label="Number of Questions"
+    label="How many questions do you want to create?"
     placeholder="10"
-    min="1"
-    max="20"/>
+    :min="1"
+    :max="20"
+    :minMsg="val => `Haiya must have at least ${val} question${val > 1 ? 's' : ''}.`"
+    :maxMsg="val => `Haiya maximum ${val} question${val > 1 ? 's' : ''}.`"
+    />
 </template>
 
 <script>
