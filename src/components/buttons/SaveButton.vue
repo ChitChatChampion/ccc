0a660b8218e7182acfe119e3233e068b93210a4b
Izz Hafeez <izzhafeez@gmail.com>
2023-09-21 14:30:51 +0800

    Edit SaveButton.vue: Organise components into folders

---
+++ src/components/buttons/SaveButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <button @click="onClick" class="bg-dt text-light px-2 grid place-content-center hover:bg-dt-light">
+    <v-icon><font-awesome-icon icon="fas fa-save"/></v-icon>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'SaveButton',
+  props: {
+    onClick: {}
+  }
+}
+</script>
