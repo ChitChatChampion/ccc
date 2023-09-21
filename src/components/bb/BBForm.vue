3e9ab6546aec006d2a8c0700b9082d41fb23ad5d
Izz Hafeez <izzhafeez@gmail.com>
2023-09-21 14:30:51 +0800

    Edit BBForm.vue: Organise components into folders

---
+++ src/components/bb/BBForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <NumericInput
+    name="number_of_prompts"
+    ref="number_of_prompts"
+    label="How many prompts do you want to create?"
+    placeholder="10"
+    :min="1"
+    :max="20"
+    :minMsg="val => `Haiya must have at least ${val} prompt${val > 1 ? 's' : ''}.`"
+    :maxMsg="val => `Haiya maximum ${val} prompt${val > 1 ? 's' : ''}.`"
+    />
+</template>
+
+<script>
+import NumericInput from '@/components/inputs/NumericInput.vue';
+
+export default {
+  name: 'BBForm',
+  components: { NumericInput },
+  methods: {
+    getValues() {
+      return {
+        number_of_prompts: this.$refs.number_of_prompts.value,
+      }
+    },
+    setValues({ number_of_prompts }) {
+      this.$refs.number_of_prompts.value = number_of_prompts;
+    }
+  }
+}
+</script>
