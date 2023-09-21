d5e58830705ee2aabb094fcff81d88c3bf4e7b14
Izz Hafeez <izzhafeez@gmail.com>
2023-09-21 14:01:00 +0800

    Edit DeleteButton.vue: Add logic for questions

---
+++ src/components/DeleteButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <button @click="onClick" class="bg-ns text-light rounded-r-lg px-2 grid place-content-center hover:bg-ns-light">
+    <v-icon><font-awesome-icon icon="fas fa-trash"/></v-icon>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'DeleteButton',
+  props: {
+    onClick: {}
+  }
+}
+</script>
