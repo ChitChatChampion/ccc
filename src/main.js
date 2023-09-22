218b651277d9aebf8856406dce4b0122595b1528
Jason Christopher <jason.christopher@u.nus.edu>
2023-09-22 13:33:46 +0800

    Edit main.js: Setup PrimeVue

--- src/main.js
+++ src/main.js
@@ -1,33 +1,42 @@
-import { createApp } from 'vue';
-import { plugin, defaultConfig } from '@formkit/vue';
-import vue3GoogleLogin from 'vue3-google-login';
-import VueSweetalert2 from 'vue-sweetalert2';
-import 'sweetalert2/dist/sweetalert2.min.css';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faArrowLeft, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
-import { faClipboard } from '@fortawesome/free-regular-svg-icons';
-import config from '../formkit.config.js';
-import App from './App.vue';
-import router from './router';
+import App from "./App.vue";
+import config from "../formkit.config.js";
+import PrimeVue from "primevue/config";
+import router from "./router";
+import Tailwind from "primevue/passthrough/tailwind";
+import vue3GoogleLogin from "vue3-google-login";
+import VueSweetalert2 from "vue-sweetalert2";
 
-import './index.css'
+import "./index.css";
+import "sweetalert2/dist/sweetalert2.min.css";
+
+import { createApp } from "vue";
+import { plugin, defaultConfig } from "@formkit/vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faClipboard } from "@fortawesome/free-regular-svg-icons";
+import {
+  faArrowLeft,
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 const app = createApp(App);
-const clientId = '785838083864-vljpaavprlvsq83slqjtf46oop9qtcfd.apps.googleusercontent.com';
+const clientId =
+  "785838083864-vljpaavprlvsq83slqjtf46oop9qtcfd.apps.googleusercontent.com";
 
 app.use(vue3GoogleLogin, {
-  clientId: clientId
+  clientId: clientId,
 });
 app.use(router);
 app.use(plugin, defaultConfig(config));
 app.use(VueSweetalert2);
+app.use(PrimeVue, { unstyled: true, pt: Tailwind });
 
 library.add(faArrowLeft);
 library.add(faChevronLeft);
 library.add(faChevronRight);
 library.add(faClipboard);
 
-app.component('font-awesome-icon', FontAwesomeIcon);
+app.component("font-awesome-icon", FontAwesomeIcon);
 
-app.mount('#app');
+app.mount("#app");
