362b82da0f2de314c0f8c8b0e8fe17e3157b9b94
Jason Christopher <jason.christopher@u.nus.edu>
2023-09-26 01:28:08 +0800

    Edit BBLanding.vue

--- src/views/gameModes/bb/BBLanding.vue
+++ src/views/gameModes/bb/BBLanding.vue
@@ -2,14 +2,14 @@
 
 <template>
   <div class="h-screen bg-gradient-to-b from-ns-light via-ns to-ns-dark z-0">
-    <NavBar backLink="/browse"/>
-    
+    <NavBar backLink="/browse" />
+
     <section class="h-2/3 w-screen z-10 grid place-content-center">
       <div class="bg-light rounded-3xl max-w-lg p-5 z-10 grid gap-5 m-10">
         <h1 class="text-3xl font-bold text-ns">Burning Bridges</h1>
         {{ instructions }}
-        <PINInput :withBackground="false" :limitWidth="false"/>
-        <OrangeButton :onClick="e => this.$router.push('/bb/create')" text="Create Game"/>
+        <PINInput :withBackground="false" :limitWidth="false" />
+        <OrangeButton :onClick="e => this.$router.push('/bb/create')" text="Create Game" />
       </div>
     </section>
 
@@ -25,7 +25,7 @@ import PINInput from '@/components/inputs/PINInput.vue';
 import OrangeButton from '@/components/buttons/OrangeButton.vue';
 
 export default {
-  name: 'CSCLanding',
+  name: 'BBLanding',
   data() {
     return {
       instructions: gameModeDict.bb.instructions,
@@ -34,9 +34,9 @@ export default {
   },
   components: {
     NavBar,
-  PINInput,
+    PINInput,
     OrangeButton,
-}
+  }
 }
 </script>
 
@@ -46,11 +46,14 @@ export default {
   top: 65%;
   left: 100%;
   transform: translate(-100%, -50%);
-  width: 30vh; /* Adjust the size of the circle as needed */
-  height: 60vh; /* The width and height should be equal for a circle */
+  width: 30vh;
+  /* Adjust the size of the circle as needed */
+  height: 60vh;
+  /* The width and height should be equal for a circle */
   border-top-left-radius: 60vh;
   border-bottom-left-radius: 60vh;
-  z-index: 0; /* Place the circle behind other content */
+  z-index: 0;
+  /* Place the circle behind other content */
   opacity: 20%;
 }
 
@@ -59,10 +62,13 @@ export default {
   top: 20%;
   left: 0%;
   transform: translate(-50%, -50%) rotate(45deg);
-  width: 50vmax; /* Adjust the size of the circle as needed */
-  height: 50vmax; /* The width and height should be equal for a circle */
-  border-radius: 4rem; /* Creates a circle by setting border-radius to 50% */
-  z-index: 0; /* Place the circle behind other content */
+  width: 50vmax;
+  /* Adjust the size of the circle as needed */
+  height: 50vmax;
+  /* The width and height should be equal for a circle */
+  border-radius: 4rem;
+  /* Creates a circle by setting border-radius to 50% */
+  z-index: 0;
+  /* Place the circle behind other content */
   opacity: 20%;
-}
-</style>
+}</style>
