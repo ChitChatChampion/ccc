dcc2680b60116f3071adbade3dfedc461e2dc0eb
Izz Hafeez <izzhafeez@gmail.com>
2023-09-26 19:14:52 +0800

    Edit QuizForm.vue: Translate React code to Vue

---
+++ src/components/quiz/QuizForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <NumericInput
+    name="number_of_questions"
+    ref="number_of_questions"
+    label="How many questions do you want to create?"
+    placeholder="10"
+    :min="1"
+    :max="20"
+    :minMsg="val => `Haiya must have at least ${val} question${val > 1 ? 's' : ''}.`"
+    :maxMsg="val => `Haiya maximum ${val} question${val > 1 ? 's' : ''}.`"
+    />
+</template>
+
+<script>
+import NumericInput from '@/components/inputs/NumericInput.vue';
+
+export default {
+  name: 'BBForm',
+  components: { NumericInput },
+  methods: {
+    getValues() {
+      return {
+        number_of_questions: this.$refs.number_of_questions.value,
+      }
+    },
+    setValues({ numberOfQuestions }) {
+      this.$refs.number_of_questions.value = numberOfQuestions;
+    }
+  }
+}
+</script>
