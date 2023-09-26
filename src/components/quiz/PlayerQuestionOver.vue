24df28f400bc0baeed5eb45e259fa94925fd44a3
Izz Hafeez <izzhafeez@gmail.com>
2023-09-26 19:14:52 +0800

    Edit PlayerQuestionOver.vue: Translate React code to Vue

---
+++ src/components/quiz/PlayerQuestionOver.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div v-if="answeredCorrect" class="answer correct">
+      <h1 class="answer-indicator">Correct</h1>
+      <Check />
+    </div>
+    <div v-else class="answer incorrect">
+      <h1 class="answer-indicator">Incorrect</h1>
+      <Incorrect />
+    </div>
+  </div>
+</template>
+
+<script>
+import Check from "@/components/animations/Check.vue";
+import Incorrect from "@/components/animations/Incorrect.vue";
+
+export default {
+  props: {
+    answeredCorrect: Boolean,
+  },
+  components: {
+    Check,
+    Incorrect,
+  },
+};
+</script>
+
+<style scoped>
+/* Add your component-specific styles here */
+</style>
