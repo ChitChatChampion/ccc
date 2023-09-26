643a2625eb98b8f494498e3b7ed3573c9210dc47
Izz Hafeez <izzhafeez@gmail.com>
2023-09-26 14:51:48 +0800

    Edit BBCreate.vue: Edit game page descriptions

--- src/views/gameModes/bb/BBCreate.vue
+++ src/views/gameModes/bb/BBCreate.vue
@@ -37,7 +37,8 @@ export default {
   name: 'BBCreate',
   setup() {
     useMeta({
-      title: 'Burning Bridges'
+      title: 'Burning Bridges',
+      description: 'Burning Bridges is an activity that stirs up lively conversations with AI prompts that might even challenge friendships. Dive in and see if your bonds can withstand the heat!'
     })
   },
   data() {
