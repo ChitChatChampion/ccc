0433281072e6379695f72fc8a7d7cd20bc6a2844
Izz Hafeez <izzhafeez@gmail.com>
2023-09-27 00:07:19 +0800

    Edit QuestionPreview.vue: Fix switch case

--- src/components/bb/QuestionPreview.vue
+++ src/components/bb/QuestionPreview.vue
@@ -52,6 +52,7 @@ export default {
         .then(response => {
           switch (response.status) {
             case 200:
+            case 201:
               return response.data;
             default:
               throw new Error('Bad method!');
@@ -72,6 +73,7 @@ export default {
         .then(response => {
           switch (response.status) {
             case 200:
+            case 201:
               return response.data;
             default:
               throw new Error('Bad method!');
