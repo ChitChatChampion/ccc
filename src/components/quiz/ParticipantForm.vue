8569e12f0ca7d2f0240ceabef36801e17655fbdd
Izz Hafeez <izzhafeez@gmail.com>
2023-09-26 19:14:52 +0800

    Edit ParticipantForm.vue: Translate React code to Vue

---
+++ src/components/quiz/ParticipantForm.vue
@@ -0,0 +1,32 @@
+<!-- /quiz/:id, not owner -->
+
+<template>
+  <TextInput
+    placeholder=""
+    ref="name"
+    label="What's your name?"
+    name="name"/>
+  <TextInput
+    placeholder=""
+    ref="information"
+    label="Tell us about yourself! The more you tell, the better the generated questions will be!"
+    name="information"/>
+</template>
+
+<script>
+import OrangeButton from "@/components/buttons/OrangeButton";
+import TextInput from "@/components/inputs/TextInput";
+
+export default {
+  name: 'ParticipantForm',
+  components: { TextInput, OrangeButton },
+  methods: {
+    getValues() {
+      return {
+        name: this.$refs.name.value,
+        information: this.$refs.information.value
+      }
+    }
+  }
+}
+</script>
