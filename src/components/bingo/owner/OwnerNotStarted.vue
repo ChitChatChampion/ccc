f10ff8259402bb6c001bfecc17305a2d5f0a789d
Izz Hafeez <izzhafeez@gmail.com>
2023-09-28 00:41:40 +0800

    Edit OwnerNotStarted.vue: Implement logic for Bingo routes

---
+++ src/components/bingo/owner/OwnerNotStarted.vue
@@ -0,0 +1,15 @@
+<template>
+  OwnerNotStarted
+  <BingoBoard/>
+  <PlayersComponent/>
+</template>
+
+<script>
+import BingoBoard from '../board/BingoBoard.vue';
+import PlayersComponent from './PlayersComponent.vue';
+
+export default {
+  name: "OwnerNotStarted",
+  components: { BingoBoard, PlayersComponent }
+}
+</script>
