0cceb69fa74b8729e86c4b12dff707a0eee40cd0
Izz Hafeez <izzhafeez@gmail.com>
2023-09-27 22:17:39 +0800

    Edit PINInput.vue: Create Bingo

--- src/components/inputs/PINInput.vue
+++ src/components/inputs/PINInput.vue
@@ -1,6 +1,6 @@
 <template>
-  <form class="rounded-3xl gap-5 z-10 grid" :class="{ 'bg-light': withBackground, 'p-5': withBackground, 'max-w-sm': limitWidth }">
-    <TextInput name="roomId" ref="roomId" placeholder="Game ID"/>
+  <form class="rounded-3xl gap-5 z-10 grid mx-auto" :class="{ 'bg-light': withBackground, 'p-5': withBackground }">
+    <TextInput name="roomId" ref="roomId" placeholder="Game ID" :isCenter="true" :isBig="true"/>
     <OrangeButton :onClick="submitPIN" text="Join Game"/>
   </form>
 </template>
