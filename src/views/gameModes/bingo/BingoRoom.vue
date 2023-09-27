1db3d090c42083d3ebafae5e1e8218089a0c084f
Izz Hafeez <izzhafeez@gmail.com>
2023-09-27 22:50:12 +0800

    Edit BingoRoom.vue: Unlink Bingo

---
+++ src/views/gameModes/bingo/BingoRoom.vue
@@ -0,0 +1,24 @@
+<!-- /bingo/:id -->
+
+<template>
+  <h1>Hi</h1>
+</template>
+
+<script>
+// import axios from 'axios';
+
+export default {
+  name: "BingoRoom",
+  created() {
+    // const roomId = this.$refs.roomId.value;
+    // const url = getUrl(`room/${roomId}`);
+    // axios.get()
+    this.isOwner = false;
+  },
+  data() {
+    return {
+      isOwner: true
+    }
+  }
+}
+</script>
