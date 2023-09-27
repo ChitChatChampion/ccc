10495b848deb7664069a14012f36c2fec4a6190f
Izz Hafeez <izzhafeez@gmail.com>
2023-09-28 00:41:40 +0800

    Edit OwnerStarted.vue: Implement logic for Bingo routes

---
+++ src/components/bingo/owner/OwnerStarted.vue
@@ -0,0 +1,15 @@
+<template>
+  OwnerStarted
+  <LeaderBoard/>
+  <BingoBoard/>
+</template>
+
+<script>
+import BingoBoard from '../board/BingoBoard.vue';
+import LeaderBoard from './LeaderBoard.vue';
+
+export default {
+  name: "OwnerStarted",
+  components: { LeaderBoard, BingoBoard }
+}
+</script>
