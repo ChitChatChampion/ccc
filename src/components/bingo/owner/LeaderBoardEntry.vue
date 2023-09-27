2329f7779365a4cc7676ef7022b9a343aa41b477
Izz Hafeez <izzhafeez@gmail.com>
2023-09-28 00:41:40 +0800

    Edit LeaderBoardEntry.vue: Implement logic for Bingo routes

---
+++ src/components/bingo/owner/LeaderBoardEntry.vue
@@ -0,0 +1,20 @@
+<template>
+  LeaderBoardEntry
+  {{ name }}: {{ score }}
+</template>
+
+<script>
+export default {
+  name: "LeaderBoardEntry",
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    score: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
