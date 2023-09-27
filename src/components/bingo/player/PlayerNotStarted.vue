022064e4bf1ab7701737f66ccbc954450297efc9
Izz Hafeez <izzhafeez@gmail.com>
2023-09-28 00:41:40 +0800

    Edit PlayerNotStarted.vue: Implement logic for Bingo routes

---
+++ src/components/bingo/player/PlayerNotStarted.vue
@@ -0,0 +1,24 @@
+<template>
+  PlayerNotStarted
+  <PlayerSubmitted v-if="hasSubmitted"/>
+  <PlayerNotSubmitted v-else/>
+</template>
+
+<script>
+import PlayerNotSubmitted from './PlayerNotSubmitted.vue';
+import PlayerSubmitted from './PlayerSubmitted.vue';
+
+export default {
+  name: "PlayerNotStarted",
+  data() {
+    return {
+      hasSubmitted: false
+    };
+  },
+  mounted() {
+    const roomId = this.$route.params.id;
+    this.hasSubmitted = !!localStorage.getItem("hasSubmitted") && !!localStorage.getItem("hasSubmitted")[roomId];
+  },
+  components: { PlayerSubmitted, PlayerNotSubmitted }
+}
+</script>
