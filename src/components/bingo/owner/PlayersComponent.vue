e6fb866d54d775a6890de8661e71aeefe994e261
Izz Hafeez <izzhafeez@gmail.com>
2023-09-28 00:41:40 +0800

    Edit PlayersComponent.vue: Implement logic for Bingo routes

---
+++ src/components/bingo/owner/PlayersComponent.vue
@@ -0,0 +1,48 @@
+<template>
+  Players
+  <PlayerPreview
+    v-for="(player, index) in players"
+    :key="player"
+    :name="player"
+    :index="index"/>
+  <OrangeButton/>
+</template>
+
+<script>
+import { getHeader, getUrl } from "@/services";
+import axios from "axios";
+import PlayerPreview from "./PlayerPreview.vue";
+import OrangeButton from "@/components/buttons/OrangeButton.vue";
+
+export default {
+  name: "PlayersComponent",
+  data() {
+    return {
+      players: []
+    };
+  },
+  mounted() {
+    const roomId = this.$refs.params.id;
+    const headers = getHeader();
+    const url = getUrl(`bingo/${roomId}/players`);
+    axios.get(url, { headers })
+      .then(response => {
+      switch (response.status) {
+        case 200:
+        case 201:
+          return response.data;
+        default:
+          throw new Error("Bad method!");
+      }
+    })
+      .then(data => {
+      this.players = data.players;
+    })
+      .catch(err => {
+      console.log(err);
+      this.$swal.fire("Oops...", "Could not retrieve player data!", "error");
+    });
+  },
+  components: { PlayerPreview, OrangeButton }
+}
+</script>
