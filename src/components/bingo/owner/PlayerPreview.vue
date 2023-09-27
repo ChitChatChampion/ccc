6c259d1d1f671454b18bb49435661d0196f7b401
Izz Hafeez <izzhafeez@gmail.com>
2023-09-28 00:41:40 +0800

    Edit PlayerPreview.vue: Implement logic for Bingo routes

---
+++ src/components/bingo/owner/PlayerPreview.vue
@@ -0,0 +1,16 @@
+<template>
+  PlayerPreview
+  {{ name }}
+</template>
+
+<script>
+export default {
+  name: "PlayerPreview",
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
