97f3de1d5fc8d87422d4c1465594268b69ac1811
Izz Hafeez <izzhafeez@gmail.com>
2023-09-28 00:41:40 +0800

    Edit BingoSquare.vue: Implement logic for Bingo routes

---
+++ src/components/bingo/board/BingoSquare.vue
@@ -0,0 +1,37 @@
+<template>
+  BingoSquare
+</template>
+
+<script>
+export default {
+  name: "BingoSquare",
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    allNames: {}
+  },
+  data() {
+    return {
+      guess: ""
+    }
+  },
+  methods: {
+    evaluate() {
+      if (this.guess === this.name) {
+        return 1;
+      }
+      return 0;
+    }
+  }
+}
+</script>
