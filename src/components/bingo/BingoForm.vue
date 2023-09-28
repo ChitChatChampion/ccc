366cf2cda269e207788773dd34cd76dbbfb05d80
Izz Hafeez <izzhafeez@gmail.com>
2023-09-28 21:45:27 +0800

    Edit BingoForm.vue: Improve design of game modes

--- src/components/bingo/BingoForm.vue
+++ src/components/bingo/BingoForm.vue
@@ -1,8 +1,8 @@
 <!-- Form for host before creating the room -->
 
 <template>
-  <h1 class="font-bold text-3xl text-ne max-w-md mx-auto">Information</h1>
-  <ul class="bg-light shadow-md rounded-lg px-8 pt-6 pb-8 mb-4 grid gap-5 max-w-md mx-auto">
+  <h1 class="font-bold text-3xl text-ne max-w-md my-5">Information</h1>
+  <ul class="bg-light shadow-md rounded-lg px-8 pt-6 pb-8 mb-4 grid gap-5 max-w-md">
     <FieldPreview
       v-for="(field, index) in fields"
       :key="field.id"
