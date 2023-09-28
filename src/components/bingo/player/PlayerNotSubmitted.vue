582d1ed30072773d39f18b590673588109de8ab0
Izz Hafeez <izzhafeez@gmail.com>
2023-09-28 19:40:40 +0800

    Edit PlayerNotSubmitted.vue: Improve bingo design significantly

--- src/components/bingo/player/PlayerNotSubmitted.vue
+++ src/components/bingo/player/PlayerNotSubmitted.vue
@@ -1,6 +1,12 @@
 <template>
-  PlayerNotSubmitted
-  <PlayerForm/>
+  <div class="w-full">
+    <div class="absolute w-full z-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-2">
+      <PlayerForm/>
+    </div>
+  </div> 
+
+  <div class="background-circle-bingo bg-ne-v-light"></div>
+  <div class="background-diamond-bingo bg-ne-v-light"></div>
 </template>
 
 <script>
@@ -11,3 +17,36 @@ export default {
   components: { PlayerForm }
 }
 </script>
+
+<style scoped>
+.background-circle-bingo {
+  position: absolute;
+  top: 65%;
+  left: 100%;
+  transform: translate(-100%, -50%);
+  width: 30vh;
+  /* Adjust the size of the circle as needed */
+  height: 60vh;
+  /* The width and height should be equal for a circle */
+  border-top-left-radius: 60vh;
+  border-bottom-left-radius: 60vh;
+  /* z-index: 0; */
+  /* Place the circle behind other content */
+  opacity: 20%;
+}
+
+.background-diamond-bingo {
+  position: absolute;
+  top: 20%;
+  left: 0%;
+  transform: translate(-50%, -50%) rotate(45deg);
+  width: 50vmax;
+  /* Adjust the size of the circle as needed */
+  height: 50vmax;
+  /* The width and height should be equal for a circle */
+  border-radius: 4rem;
+  /* Creates a circle by setting border-radius to 50% */
+  /* z-index: 0; */
+  /* Place the circle behind other content */
+  opacity: 20%;
+}</style>
