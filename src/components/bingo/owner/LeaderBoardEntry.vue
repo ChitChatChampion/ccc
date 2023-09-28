aaa11e065339cd9dae7f5e06f5dd0acf0f73607d
Izz Hafeez <izzhafeez@gmail.com>
2023-09-28 18:26:42 +0800

    Edit LeaderBoardEntry.vue: Basic styling

--- src/components/bingo/owner/LeaderBoardEntry.vue
+++ src/components/bingo/owner/LeaderBoardEntry.vue
@@ -1,6 +1,8 @@
 <template>
-  LeaderBoardEntry
-  {{ name }}: {{ score }}
+  <div class="bg-ne-light flex flex-row place-content-between p-3 text-light">
+    <h2>{{ name }}</h2>
+    <h2 class="w-5 text-center">{{ score }}</h2>
+  </div>
 </template>
 
 <script>
