505373edc75930c29cc0ba0016abf7f76a8cc611
Izz Hafeez <izzhafeez@gmail.com>
2023-09-28 14:22:14 +0800

    Fix bingo board

--- src/components/bingo/board/BingoBoard.vue
+++ src/components/bingo/board/BingoBoard.vue
@@ -51,7 +51,7 @@ export default {
   methods: {
     evaluate() {
       let score = 0;
-      for (square of this.squares) {
+      for (let square of this.squares) {
         const name = square.name;
         score += this.$refs[name].evaluate();
       }
