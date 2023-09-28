35c562c86180a19bb2fc8b0a4cb121170096f5b9
Izz Hafeez <izzhafeez@gmail.com>
2023-09-29 00:42:03 +0800

    Edit loginToGoogle.js: Fix bugs in modals

--- src/components/loginToGoogle.js
+++ src/components/loginToGoogle.js
@@ -11,14 +11,6 @@ const loginToGoogle = ({ redirect, router }) => {
         localStorage.setItem('name', data.name);
         localStorage.setItem('expiry', Date.now() + 3600000);
         router.push(redirect);
-        this.$swal.fire({
-          toast: true,
-          position: 'bottom',
-          icon: 'success',
-          title: 'Field has been deleted!',
-          showConfirmButton: false,
-          timer: 1500
-        });
       });
   });
 }
