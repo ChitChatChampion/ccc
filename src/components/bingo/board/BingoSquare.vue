10bd902204f539d999aebcc231d8c50b4e138ed8
Izz Hafeez <izzhafeez@gmail.com>
2023-09-28 17:43:05 +0800

    Edit BingoSquare.vue: Integrate FE and BE

--- src/components/bingo/board/BingoSquare.vue
+++ src/components/bingo/board/BingoSquare.vue
@@ -1,5 +1,10 @@
 <template>
-  BingoSquare
+  <button
+    class="bg-ne text-light w-40 h-40"
+    @click="showPopup">
+    <h1 class="bold">{{ title }}</h1>
+    {{ guess }}
+  </button>
 </template>
 
 <script>
@@ -18,7 +23,10 @@ export default {
       type: String,
       required: true
     },
-    allNames: {}
+    allNames: {},
+    index: {
+      type: Number
+    }
   },
   data() {
     return {
@@ -31,6 +39,23 @@ export default {
         return 1;
       }
       return 0;
+    },
+    showPopup() {
+      const options = {};
+      for (let name of this.allNames) {
+        options[name] = name;
+      }
+      this.$swal.fire({
+        title: this.title,
+        text: this.description,
+        input: 'select',
+        inputOptions: options,
+        inputPlaceholder: 'Who fits this description?',
+        showCancelButton: true,
+        confirmButtonText: "Confirm"
+      }).then(result => {
+        this.guess = result.value;
+      });
     }
   }
 }
