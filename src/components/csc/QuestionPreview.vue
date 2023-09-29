5b4fd7cd8831a2767651d94a063777aa1a2caa4b
Izz Hafeez <izzhafeez@gmail.com>
2023-09-29 15:41:50 +0800

    Edit QuestionPreview.vue: Remove annoying modals when CRUDding

--- src/components/csc/QuestionPreview.vue
+++ src/components/csc/QuestionPreview.vue
@@ -47,12 +47,6 @@ export default {
   components: { DeleteButton },
   methods: {
     saveQuestion() {
-      this.$swal.fire({
-        title: "Saving Question...",
-        didOpen: () => {
-          this.$swal.showLoading();
-        }
-      });
       const headers = getHeader();
       const url = getUrl(`csc/questions/${this.id}`);
       axios.put(url, { content: this.value }, { headers })
@@ -66,8 +60,6 @@ export default {
           }
         })
         .then(() => {
-          // this.$swal.fire('Success!', 'Question has been saved!', 'success');
-          this.$swal.close();
           this.$swal.fire({
             toast: true,
             position: 'bottom',
@@ -83,12 +75,6 @@ export default {
         });
     },
     deleteQuestion() {
-      this.$swal.fire({
-        title: "Deleting Question...",
-        didOpen: () => {
-          this.$swal.showLoading();
-        }
-      });
       const headers = getHeader();
       const url = getUrl(`csc/questions/${this.id}`);
       axios.delete(url, { headers })
@@ -102,8 +88,6 @@ export default {
           }
         })
         .then(() => {
-          // this.$swal.fire('Success!', 'Question has been deleted!', 'success');
-          this.$swal.close();
           this.$swal.fire({
             toast: true,
             position: 'bottom',
